8ceab5f0f44c0688fe0fb9046388bcc0e5ecf020
mmassaroni <miguelmassaroni@gmail.com>
2017-05-17 22:37:49 -0300

    Creación del Proyecto "Lucy"

---
+++ Lucy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lucy.Startup))]
+namespace Lucy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
