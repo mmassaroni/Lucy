b735a83765fb60547a8f3a486ef791149c0841f1
mmassaroni <miguelmassaroni@gmail.com>
2017-06-20 10:29:07 -0300

    zzz

--- Lucy/App_Start/BundleConfig.cs
+++ Lucy/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Lucy
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/site.css",
                       "~/Content/site.css"));
         }
     }
